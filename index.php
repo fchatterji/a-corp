289a51d8514a513ec988efc123560f2888e7c5c0
Florent <florent.chatterji@gmail.com>
2016-12-27 18:46:59 +0100

    réécriture du routeur

--- index.php
+++ index.php
@@ -1,26 +1,9 @@
 <?php 
-
-session_start();
-
-
-require_once "Outils\Autoloader.php";
+require_once "Tools\Autoloader.php";
 
 Autoloader::register();
 
-if(isset($_GET['page']))
-{
-    $page = $_GET['page'];
-    
-    if(isset($_GET['action']))
-    {
-        $action = $_GET['action'];
-    }
-    else
-    {
-        $action = "index";
-    }
-    Router::Route($page, $action);
-}
+Router::route();
 
 ?>
 
