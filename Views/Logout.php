7f756f448847dafa6efacec56412daa2bb6cdedd
Florent <florent.chatterji@gmail.com>
2016-12-28 18:01:36 +0100

    renamed view files to include "View" at the end

--- Views/Logout.php
+++
@@ -1,8 +0,0 @@
-<?php include "header.php";?>
-
-<form action="" method="post">
-    <input type="submit" value="Logout" />
-</form>
-
-<?php include "footer.php";?>
-
--- Views/SignOut.php
+++
@@ -1,8 +0,0 @@
-<?php include "header.php";?>
-
-<form action="" method="post">
-    <input type="submit" value="signout" />
-</form>
-
-<?php include "footer.php";?>
-
