81caf1fb027b55176abb4350c6b130c1c5dc4bcb
Florent <florent.chatterji@gmail.com>
2016-12-30 16:17:39 +0100

    refactor: added partials instead of having everything in index.php

--- Partials/footer.php
+++ Partials/footer.php
@@ -1,2 +1,7 @@
+<!-- close container -->
+</div>
+
+</body>
 
 </html>
+
--- Partials/header.php
+++ Partials/header.php
@@ -1,23 +1,61 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <title>A-corp</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="css/styles.css">
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
+    <!-- datepicker -->
+    <link  href="/a-corp/datepicker/datepicker.css" rel="stylesheet">
+    <script src="/a-corp/datepicker/datepicker.js"></script>
+
+    <!-- bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
 
 </head>
 
 <body>
-    <header>
-        <br>
-        <a href="home">home</a>
-        <a href="login">login</a>
-        <a href="logout">logout</a>
-        <a href="register">register</a>
-        <a href="salles">liste des salles</a>
-        <br>
-    </header>
+    <div class="container">
 
-</body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brand</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="home">Accueil <span class="sr-only">(current)</span></a></li>
+                        <li><a href="/a-corp/login">Login</a></li>
+                        <li><a href="/a-corp/logout">Logout</a></li>
+                        <li><a href="/a-corp/register">Register</a></li>
+                        <li><a href="/a-corp/salles">Liste des salles</a></li>
+                        <li><a href="/a-corp/reservations">Liste des reservations</a></li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Link</a></li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+
