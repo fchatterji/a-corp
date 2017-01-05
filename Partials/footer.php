dc9b00da030af8fdb1a5d5a46d688acfe20049cd
Florent <florent.chatterji@gmail.com>
2017-01-05 23:59:33 +0100

    refactor: added description tag, optmized order of scripts, added async and defer

--- Partials/footer.php
+++ Partials/footer.php
@@ -28,13 +28,6 @@
 
     </script>
 
-    <!-- bootstrap JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    <!-- Font Awesome -->
-    <script src="https://use.fontawesome.com/ac39aecef6.js"></script>
-
-
 </body>
 
 </html>
--- Partials/header.php
+++ Partials/header.php
@@ -5,20 +5,28 @@
     <title>A-corp</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Jquery -->
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
-
-    <!-- Datepicker -->
-    <link href="/datepicker/datepicker.min.css" rel="stylesheet">
-    <script src="/datepicker/datepicker.min.js"></script>
+    <meta name="description" content="A simple booking system"/>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
+    <!-- Datepicker CSS -->
+    <link href="/datepicker/datepicker.min.css" rel="stylesheet">
+
     <!-- stylesheet -->
     <link href="/CSS/style.css" rel="stylesheet">
 
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous" async defer></script>
+
+    <!-- Datepicker JS -->
+    <script src="/datepicker/datepicker.min.js" async defer></script>
+
+    <!-- bootstrap JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous" async defer></script>
+
+    <!-- favicon -->
+     <link rel="icon" href="/Images/favicon.png">
 
 </head>
 
